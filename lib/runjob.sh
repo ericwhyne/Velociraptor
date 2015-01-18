5b7ad477f1c407f1b2d00ff117df93a49e2b6368
Eric Whyne <ericwhyne@gmail.com>
2015-01-17 23:30:07 -0500

    Update runjob.sh

--- lib/runjob.sh
+++ lib/runjob.sh
@@ -2,9 +2,10 @@
 export EC2_HOME=`pwd`/lib/ec2-api-tools-1.7.3.0
 export PATH=$PATH:$EC2_HOME/bin
 
-# usage: runjob jobfile.sh aws_key_file
+# usage: runjob jobfile.sh aws_key_file instance_key
 $jobfile=$1
 $aws_keys_file=$2
+$instance_key=$3
 
 # Ubuntu Server 14.04 LTS (PV), SSD Volume Type - ami-98aa1cf0
 ec2-run-instances \
@@ -14,6 +15,6 @@ ec2-run-instances \
   --associate-public-ip-address true \
   --instance-initiated-shutdown-behavior terminate \
   --instance-type t1.micro \
-  -k genericworkkey \
+  -k $instance_key \
   --user-data-file $jobfile \
   ami-98aa1cf0
